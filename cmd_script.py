26b414cf38487d6262920c0dfc53d7824808b85d
Brian Lee <blee025432@gmail.com>
2021-10-06 09:41:30 -0400

    slight cmd modifications

--- cmd_script.py
+++ cmd_script.py
@@ -1,7 +1,23 @@
 import requests
+import argparse
+import pdb
 
 BASE_URL = "http://127.0.0.1:8000/compile/"
 
 if __name__ == "__main__":
-    x = requests.post(BASE_URL, data={}, files={'filename': open("../pc98-stuffs/PMD/songs/divine.mml", "r")})
+
+    parser = argparse.ArgumentParser(
+        description="Useful MIDI processing operations for PMD",
+        formatter_class=argparse.ArgumentDefaultsHelpFormatter)
+
+    parser.add_argument("mml_file", type=str, help="path to .mml file")
+    parser.add_argument("-o", "--output", type=str, help="optional .m2 output filename", default=None)
+
+    results = parser.parse_args()
+
+    data = {}
+    if results.output is not None:
+        data['output'] = results.output
+    x = requests.post(
+        BASE_URL, data=data, files={'filename': open(results.mml_file, "r")})
     print(x.text)
