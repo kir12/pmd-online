7593f15b06b898f7a7e621f5e3644596731beb9e
Brian Lee <blee025432@gmail.com>
2021-10-05 22:17:18 -0400

    operator problems

--- compile/views.py
+++ compile/views.py
@@ -4,20 +4,20 @@ from rest_framework import status
 from pathlib import Path
 import re
 from .models import PMDUpload
-import uuid
 
 
 # Create your views here.
 @api_view(['POST'])
 def index(request):
 
-    # (attempt to) load MML file and optional output name
+    # (attempt to) load MML file and construct output name
     try:
         file = request.FILES['filename']
         assert(Path(file.name).suffix == ".mml")
         output = request.POST.get('output')
         if output is None:
             output = file.name
+        output = Path(output).stem
         output = re.sub("[^a-zA-z0-9]", "", output)[:6]
         output = str(Path(output).with_suffix(".m2"))
     except BaseException:
@@ -25,8 +25,11 @@ def index(request):
             'error': "An invalid file was supplied."
         }, status=status.HTTP_400_BAD_REQUEST)
     
+    # initialize PMDUpload db object and call save on objects
     save_obj = PMDUpload(returned_m2_filename=output, mml_file=file)
     save_obj.save()
+
+    # run PMD
 
     return Response({
         'file': file.name,
